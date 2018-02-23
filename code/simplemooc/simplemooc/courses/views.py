3cb724f6bd0f687a301262554259eb40eaa7de0f
PuckmanXY <kayoanderson0403@gmail.com>
2018-02-22 22:29:50 -0300

    Forms introduced in courses' details page

--- code/simplemooc/simplemooc/courses/views.py
+++ code/simplemooc/simplemooc/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Course
+from .forms import ContactCourse
 
 # Create your views here.
 
@@ -21,8 +22,16 @@ def index(request):
 
 def details(request, slug):
     course = get_object_or_404(Course, slug = slug)
-    context = {
-        'course': course
-    }
+    context = {}
+    if request.method == 'POST':
+        form = ContactCourse(request.POST)
+        if form.is_valid():
+            context['is_valid'] = True
+            print(form.cleaned_data)
+            form = ContactCourse()
+    else:
+        form = ContactCourse()
+    context['form'] = form
+    context['course'] = course
     template_name = 'courses/details.html'
     return render(request, template_name, context)
